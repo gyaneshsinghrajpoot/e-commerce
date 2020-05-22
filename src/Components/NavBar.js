1240bb3845d680a87a6ea595d116638904c4badb
unknown <gyanesh2248@gmail.com>
2020-05-22 10:50:14 +0530

    adding router in index js

---
+++ src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React, { Component} from 'react';
+
+export default class NavBar extends Component{
+    render(){
+        return(
+            <div>
+                <h3>hello from NavBar</h3>
+            </div>
+        )
+    }
+}
